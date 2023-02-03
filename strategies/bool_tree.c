c40b0d047b9e8e655401744c0b50ddff3c968e37
MarcFaussurier <faussurier.marc@icloud.com>
2023-02-03 08:44:13 +0100

    fix bool_tree

--- strategies/bool_tree.c
+++ strategies/bool_tree.c
@@ -1,4 +1,84 @@
 #include "solver.h"
+#include "limits.h"
+
+int		bool_tree_r(t_stack *s, t_list **l, t_possible_instructions pi, int depth);
+
+
+int		try_s(t_stack *s, t_list **l, int depth)
+{
+	int		*mem_sc;
+	t_stack	sc;
+	int		i;
+
+	(void) l;
+	mem_sc = malloc(ARG_MAX * sizeof(int));
+	sc = stack_init(mem_sc);
+	stack_clone(&sc, s);
+	i = 0;
+	i += sa(&sc, 0, 0);
+	i += bool_tree_r(&sc, 0, (t_possible_instructions){.s=0, .r=1, .rr=1}, depth + 1);
+	free(mem_sc);
+	return (is_sorted_asc(&sc) ? i : 0);
+}
+
+int		try_r(t_stack *s, t_list **l, int depth)
+{
+	int		*mem_sc;
+	t_stack	sc;
+	int		i;
+
+	(void) l;
+	mem_sc = malloc(ARG_MAX * sizeof(int));
+	sc = stack_init(mem_sc);
+	stack_clone(&sc, s);
+	i = (ra(&sc, 0, 0));
+	i += bool_tree_r(&sc, 0, (t_possible_instructions){.s=1, .r=1, .rr=0}, depth + 1);
+	free(mem_sc);
+	return (is_sorted_asc(&sc) ? i : 0);
+}
+
+int		try_rr(t_stack *s, t_list **l, int depth)
+{
+	int		*mem_sc;
+	t_stack	sc;
+	int		i;
+
+
+	(void) l;
+	mem_sc = malloc(ARG_MAX * sizeof(int));
+	sc = stack_init(mem_sc);
+	stack_clone(&sc, s);
+	i = (rra(&sc, 0, 0));
+	i += bool_tree_r(&sc, 0, (t_possible_instructions){.s=1, .r=0, .rr=1}, depth + 1);
+	free(mem_sc);
+	return (is_sorted_asc(&sc) ? i : 0);
+}
+
+
+char bool_tree_c(t_stack *s, t_list **l, t_possible_instructions pi,
+			int depth);
+
+int		bool_tree_r(t_stack *s, t_list **l, t_possible_instructions pi, int depth)
+{
+	int	c;
+
+	if (depth > 10)
+		return 0;
+	if (is_sorted_asc(s))
+		return 0;
+	c = bool_tree_c(s, l, pi, depth);
+	if (c == 's')
+		return (sa(s, 0, l) + bool_tree_r(s, l, (t_possible_instructions) {.s=0, .r=1, .rr=1}, depth + 1));
+	
+	if (c == 'r')
+		return (ra(s, 0, l) + bool_tree_r(s, l, (t_possible_instructions) {.s=1, .r=1, .rr=0}, depth + 1));
+	
+	if (c == 'i')
+		return (rra(s, 0, l) + bool_tree_r(s, l, (t_possible_instructions) {.s=1, .r=0, .rr=1}, depth + 1));
+	
+
+	return (0);
+}
 
 int	bool_tree(t_stack *a, t_stack *b, t_list **l)
 {
@@ -6,7 +86,7 @@ int	bool_tree(t_stack *a, t_stack *b, t_list **l)
 
 	(void) b;
 	i = 0;
-	i += bool_tree_r(a, l,
-			(t_possible_instructions){.s = 1, .r = 1, .rr = 1}, 0);
+	i += bool_tree_r(a, l, (t_possible_instructions) {.s=1, .r=1, .rr=1} , 0);
 	return (i);
 }
+
--- strategies/bool_tree_utils.c
+++ strategies/bool_tree_utils.c
@@ -1,83 +1,40 @@
 #include "solver.h"
 #include "limits.h"
 
-int	try_s(t_stack *s, t_list **l, int depth)
-{
-	static int		mem_sc[ARG_MAX];
-	t_stack	sc;
-	int		i;
-
-	(void) l;
-	sc = stack_init(mem_sc);
-	stack_clone(&sc, s);
-	i = 0;
-	i += sa(&sc, 0, 0);
-	i += bool_tree_r(&sc, 0,
-			(t_possible_instructions){.s = 0, .r = 1, .rr = 1}, depth + 1);
-	if (is_sorted_asc(&sc))
-		return (i);
-	return (0);
-}
-
-int	try_r(t_stack *s, t_list **l, int depth)
-{
-	static int		mem_sc[ARG_MAX];
-	t_stack	sc;
-	int		i;
-
-	(void) l;
-	sc = stack_init(mem_sc);
-	stack_clone(&sc, s);
-	i = (ra(&sc, 0, 0));
-	i += bool_tree_r(&sc, 0,
-			(t_possible_instructions){.s = 1, .r = 1, .rr = 0}, depth + 1);
-	if (is_sorted_asc(&sc))
-		return (i);
-	return (0);
-}
-
-int	try_rr(t_stack *s, t_list **l, int depth)
-{
-	static int		mem_sc[ARG_MAX];
-	t_stack	sc;
-	int		i;
-
-	(void) l;
-	sc = stack_init(mem_sc);
-	stack_clone(&sc, s);
-	i = (rra(&sc, 0, 0));
-	i += bool_tree_r(&sc, 0,
-			(t_possible_instructions){.s = 1, .r = 0, .rr = 1}, depth + 1);
-	if (is_sorted_asc(&sc))
-		return (i);
-	return (0);
-}
-
-int	bool_tree_r(t_stack *s, t_list **l,
-	t_possible_instructions pi, int depth)
+static int	is_branch(int(*try)(t_stack *s, t_list **l, int depth), 
+		t_stack *s,
+		t_list **l,
+		int depth,
+		int *ic)
 {
 	int	success;
-	int	ic;
-	int	c;
 
-	success = 0;
-	ic = INT_MAX;
-	if (depth > 10)
-		return (0);
-	if (is_sorted_asc(s))
-		return (0);
-    c = bool_tree_get_c(s, l, pi, depth);
-	if (c == 's')
-		return (sa(s, 0, l) + bool_tree_r(s, l,
-				(t_possible_instructions){.s = 0, .r = 1, .rr = 1},
-			depth + 1));
-	if (c == 'r')
-		return (ra(s, 0, l) + bool_tree_r(s, l,
-				(t_possible_instructions){.s = 1, .r = 1, .rr = 0},
-			depth + 1));
-	if (c == 'i')
-		return (rra(s, 0, l) + bool_tree_r(s, l,
-				(t_possible_instructions){.s = 1, .r = 0, .rr = 1},
-			depth + 1));
+	success = try(s, l, depth);
+	if (success && success < *ic)
+	{
+		*ic = success;
+		return (1);
+	}
 	return (0);
+}
+
+char bool_tree_c(t_stack *s, t_list **l, t_possible_instructions pi,
+			int depth)
+{
+	char	c;
+	int		ic;
+	int		success;
+
+	ic = INT_MAX;
+	success = 0;
+	c = 0;
+	if (pi.s && is_branch(try_s, s, l, depth, &ic))
+		c = 's';
+	if (pi.r && is_branch(try_r, s, l, depth, &ic))
+		c = 'r';
+	if (pi.rr && is_branch(try_rr, s, l, depth, &ic))
+		c = 'i';
+	if (!ic)
+		c = 'r';
+	return c;
 }
